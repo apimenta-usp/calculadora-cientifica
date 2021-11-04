bc48ab3944b0fc45cad82482c7d1d67384574603
Adriano Pimenta <apimenta.usp@gmail.com>
2021-11-04 10:46:13 -0300

    Criando a classe 'ControleDeImagens' e seus métodos

---
+++ InterfaceUsuario/Personalizacao/ControleDeImagens.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace InterfaceUsuario.Personalizacao {
+    public class ControleDeImagens {
+        public static void UmaImagem(Button botao, Bitmap imagem) {
+            botao.BackColor = Color.Transparent;
+            botao.BackgroundImage = imagem;
+            botao.BackgroundImageLayout = ImageLayout.Zoom;
+            botao.ForeColor = Color.Transparent;
+        }
+
+        public static void DuasImagens(Button botao, Bitmap imagemBotao,
+                            PictureBox pictureBox, Bitmap imagemPictureBox) {
+            botao.BackColor = Color.Transparent;
+            botao.BackgroundImage = imagemBotao;
+            botao.BackgroundImageLayout = ImageLayout.Zoom;
+            botao.ForeColor = Color.Transparent;
+
+            pictureBox.BackColor = Color.Transparent;
+            pictureBox.BackgroundImage = imagemPictureBox;
+            pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox.ForeColor = Color.Transparent;
+        }
+    }
+}
