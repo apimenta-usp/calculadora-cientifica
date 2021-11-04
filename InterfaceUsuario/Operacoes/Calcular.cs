60c2c35d00ba48d7b8d9d0b93d587850e4070b51
Adriano Pimenta <apimenta.usp@gmail.com>
2021-11-04 08:53:53 -0300

    Criando a classe 'Calcular' e o método 'LimparCampos'

---
+++ InterfaceUsuario/Operacoes/Calcular.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceUsuario.Personalizacao;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InterfaceUsuario.Operacoes {
+    public class Calcular {
+        public static void LimparCampos(Label lblVisor) {
+            lblVisor.Text = string.Empty;
+            FrmCalculadoraCientifica.Numero1 = 0;
+            FrmCalculadoraCientifica.Numero2 = 0;
+            FrmCalculadoraCientifica.Operacao = string.Empty;
+            FrmCalculadoraCientifica.PressionouIgual = false;
+            FrmCalculadoraCientifica.PressionouPotenciacao = false;
+            FrmCalculadoraCientifica.PressionouExponencial = false;
+        }
+    }
+}
